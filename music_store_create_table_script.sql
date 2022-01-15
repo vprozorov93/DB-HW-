40c7b971d9c8c63740f3f780211e5c25bcf6ed70
Vladislav Prozorov <93047063+vprozorov93@users.noreply.github.com>
2022-01-15 15:38:09 +0300

    Add script of create db tables

---
+++ music_store_create_table_script.sql
@@ -0,0 +1,24 @@
+create table if not exists Genre (
+	id serial primary key,
+	Title varchar(35) not null unique);
+
+create table if not exists Singer (
+	id serial primary key,
+	Singer_name text not null,
+	Alias text not null unique,
+	id_genre serial references Genre(id));
+	
+create table if not exists Album (
+	id serial primary key,
+	Title text not null,
+	Year_of_creation date not null,
+	Info text,
+	id_singer serial references Singer(id));
+	
+create table if not exists Song (
+	id serial primary key,
+	Title text not null,
+	Duration timestamp not null,
+	id_singer serial references Singer(id),
+	id_album serial references Album(id));
+	
