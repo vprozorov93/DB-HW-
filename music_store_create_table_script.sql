dd49691cf9a4fbbf9d9496f6966fb92d517d5735
Vladislav Prozorov <prozorov93@gmail.com>
2022-01-19 20:20:41 +0300

    HomeWork 2

--- music_store_create_table_script.sql
+++ music_store_create_table_script.sql
@@ -2,23 +2,48 @@ create table if not exists Genre (
 	id serial primary key,
 	Title varchar(35) not null unique);
 
+
 create table if not exists Singer (
 	id serial primary key,
 	Singer_name text not null,
-	Alias text not null unique,
-	id_genre serial references Genre(id));
+	Alias text not null unique);
+
+
+create table if not exists SingerGenge (
+	id serial primary key,
+	singer integer references Singer(id),
+	genre integer references Genre(id));
+	
 	
 create table if not exists Album (
 	id serial primary key,
 	Title text not null,
 	Year_of_creation integer not null,
-	Info text,
-	id_singer serial references Singer(id));
+	Info text);
+
+
+create table if not exists AlbumSinger (
+	id serial primary key,
+	singer integer references Singer(id),
+	album integer references Album(id));
+	
+
+create table if not exists Collection (
+	id serial primary key,
+	Title text not null,
+	Year_of_creation integer not null);
+
 	
 create table if not exists Song (
 	id serial primary key,
 	Title text not null,
 	Duration timestamp not null,
-	id_singer serial references Singer(id),
-	id_album serial references Album(id));
+	id_album integer references Album(id));
+
+
+create table if not exists SongCollection (
+	id serial primary key,
+	song integer references Song(id),
+	collection integer references Collection(id));
+
 	
